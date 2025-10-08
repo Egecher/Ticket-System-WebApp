1d5d52b25b216e28eae136ab86a04249b6d8be1a
Egecher <egemenuysalll@gmail.com>
2025-10-08 23:27:51 +0300

    refactor: userController.createUser delete

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/user.controller')
 
 router.get('/:id', userController.getUser);
-router.post('/', userController.createUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
