8c739b4fa96436deb4671dcd02a5565b2620789c
Egecher <egemenuysalll@gmail.com>
2025-10-09 00:01:43 +0300

    refactor: mongoose methods correction

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,12 @@
 const User = require('../models/user.model');
 
 exports.getUser = async (req, res) => {
-    const user = User.getUserById(req.params.id);
+    const user = User.findById(req.params.id);
     if (!user) return res.status(404).send('User not found');
     res.json(user);
 }
 
 exports.deleteUser = async (req, res) => {
-    User.deleteUserById(req.params.id);
+    User.findOneAndDelete(req.params.id);
     res.status(204).send();
 }
