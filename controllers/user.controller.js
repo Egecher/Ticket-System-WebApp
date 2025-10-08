cb37413cfab0613d86b75c13f174b38f5553bf2f
Egecher <egemenuysalll@gmail.com>
2025-10-08 23:26:36 +0300

    refactor: createUser delete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,22 +6,6 @@ exports.getUser = async (req, res) => {
     res.json(user);
 }
 
-exports.createUser = async (req, res) => {
-    const { name, email, password, passwordAgain } = req.body;
-    if (password !== passwordAgain) {
-        return res.status(400).send("Şifreler eşleşmiyor!");
-    }
-
-    const newUser = {
-        id: Date.now().toString(),
-        name: name,
-        email: email,
-        password: password
-    };
-    User.addUser(newUser);
-    res.status(201).json(newUser);
-}
-
 exports.deleteUser = async (req, res) => {
     User.deleteUserById(req.params.id);
     res.status(204).send();
